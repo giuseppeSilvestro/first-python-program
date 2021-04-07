a0ee3b45c1c2155a1f31ddd28f51e292473ad623
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2021-04-07 19:51:51 +0100

    Keep running the program till there are no tickets left.

--- Ticket_master_kind_of.py
+++ Ticket_master_kind_of.py
@@ -4,18 +4,37 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100
 
-# Output how many tickets are left using the tickets_remaining variable
-print('There are {} tickets remaining.'.format(tickets_remaining))
+# Run this code till there are no tickets left
+while tickets_remaining > 0:
+    # Output how many tickets are left using the tickets_remaining variable
+    print('There are {} tickets remaining.'.format(tickets_remaining))
 
 
-# Gather the user's name and assign it to a new variable
-username = input('What is your name? ')
+    # Gather the user's name and assign it to a new variable
+    username = input('What is your name? ')
 
-# Prompt the user by name and ask how many tickets they would like
-number_of_thickets = int(input('Hello {}! How many tickets would you like? '.format(username)))
+    # Prompt the user by name and ask how many tickets they would like
+    number_of_tickets = int(input('Hello {}! How many tickets would you like? '.format(username)))
 
-# Calculate the price for the total number of thickets
-total_price = number_of_thickets * TICKET_PRICE
+    # Calculate the price for the total number of thickets
+    total_price = number_of_tickets * TICKET_PRICE
 
-# Output the price to the screen
-print('If you decide to buy the tickets, the total price will be {}'.format(total_price))
+    # Output the price to the screen
+    print('If you decide to buy the tickets, the total price will be {}'.format(total_price))
+
+    # Prompt user if the want to proceed
+    user_choice = input('{} would you like to proceed with your order? Y/N '.format(username))
+    # If the want to proceed
+    if user_choice.lower() == 'y':
+        # print out to the screen "SOLD!"
+        print('SOLD!')
+        # reduce the tickets remaining
+        tickets_remaining -= number_of_tickets
+    # Otherwise
+    else:
+        print('Your order has not been processed.')
+    # thank them by name
+    print('Thank you for stopping by, {}'.format(username))
+
+# notify the user that the tickets are sold out
+print('There are no tickets left!')
